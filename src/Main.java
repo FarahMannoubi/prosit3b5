668e0e0301449c8f8182b15d8b57d4aead9e21b9
macbook <farah.mannoubi@esprit.tn>
2025-04-06 11:40:26 +0100

    feat:to do prosit1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ZooManagement zm = new ZooManagement();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert the number of cages");
+
+        int numberOfCages = sc.nextInt();
+
+        System.out.println("Insert the name of the zoo");
+
+        String nameOfZoo = sc.next();
+
+        zm.nbrCages = numberOfCages;
+        zm.zooName = nameOfZoo;
+
+        zm.displayInformation();
     }
 }
